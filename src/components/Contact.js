abab5ce56a2c0aef88621c661df831c36fa72b8f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-06 18:23:02 -0500

    Replaced hard coded info in Contact component with props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
         <div className="contact-card">
-        <img src="https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="cat img"/>
-        <h3>Mr. Whiskerson</h3>
+        <img src={props.img} alt="cat img"/>
+        <h3>{props.name}</h3>
         <div className="info-group">
             {/* <img src="./images/phone-icon.png" /> */}
-            <p>(212) 555-1234</p>
+            <p>{props.phone}</p>
         </div>
         <div className="info-group">
             {/* <img src="./images/mail-icon.png" /> */}
-            <p>mr.whiskaz@catnap.meow</p>
+            <p>{props.email}</p>
         </div>
     </div>
     );
